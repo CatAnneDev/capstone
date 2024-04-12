5a3e18ef1de99139de4cd6d3a4e08bb64f12026c
Catherine <62034499+CatAnneDev@users.noreply.github.com>
2024-04-12 08:33:35 -0500

    updated comments

--- delete_user.php
+++ delete_user.php
@@ -1,18 +1,13 @@
 <?php
     $id = $_GET['id'];
-    //Connect DB
-    //Create query based on the ID passed from you table
-    //query : delete where Staff_id = $id
-    // on success delete : redirect the page to original page using header() method
-
     require_once('index.php');
-    // sql to delete a record
+    // sql to delete a record where GET id = accounts id
     $sql = "DELETE FROM accounts WHERE id = $id"; 
 
     if (mysqli_query($conn, $sql)) {
-        header('Location: index.php?page=users'); //If book.php is your main page where you list your all records
+        header('Location: index.php?page=users'); 
         exit;
     } else {
-        echo "Error deleting record";
+        echo "Error: deleting record fail";
     }
 ?>
