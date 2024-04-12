5db104ca88f62d363a321c8312ea63991496b75c
Catherine <62034499+CatAnneDev@users.noreply.github.com>
2024-04-12 07:36:31 -0500

    Update history.php

--- history.php
+++ history.php
@@ -15,6 +15,10 @@ if ($_SESSION["loggedin"] != true)
 #   print
 #   add pending -> coplete button that updates inentoryphp
 # spae out
+$groupname = $_SESSION["groupname"];
+$sql = "SELECT * FROM history WHERE groupname = ?";
+$history_orders = $conn->execute_query($sql, [$groupname]);
+
 
 ?>
 
@@ -22,6 +26,22 @@ if ($_SESSION["loggedin"] != true)
 
 <div class="content-wrapper">
     <h1 class="h1-align">History</h1>
+    <?php foreach($history_orders as $row): ?>
+        <!-- set prevvious batcch nummber. If new == previous, make a new TD, else mmake new table -->
+        <p>B# <?= htmlspecialchars($row['batch_number']) ?></p>
+        <table>
+            <tr>
+                <th>Product Name</th>
+                <th>Quantity</th>
+                <th>Status</th>
+            </tr>
+            <tr>
+                <td><?= htmlspecialchars($row['product_name']) ?></td>
+                <td><?= htmlspecialchars($row['quantity']) ?></td>
+                <td><?= htmlspecialchars($row['status']) ?></td>
+            </tr>
+        </table>
+    <?php endforeach ?>
 </div>
 
 <?=footer()?>
