e5ef32462d64e616fc171c356363e7d25d686218
OpalDel <62034499+CatAnneDev@users.noreply.github.com>
2024-04-25 19:43:58 -0500

    exported mysql tables

---
+++ capstone.sql
@@ -0,0 +1,185 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 26, 2024 at 02:43 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `capstone`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `permission` varchar(50) NOT NULL,
+  `groupname` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `accounts`
+--
+
+INSERT INTO `accounts` (`id`, `username`, `password`, `permission`, `groupname`) VALUES
+(1, 'testing', '$2y$10$Q07W5LhI9ugBLQrQ/pzosOiXmld5pwSGmfsTnce4484ltjhP8D6.y', 'groupadmin', 'TestGroup'),
+(4, 'dfd', '$2y$10$pihVnwFH8/xl0Ji1f9TjlelB7kmH9xHHtWM/h9BHLiLWtHr0I2q5.', 'groupadmin', 'df'),
+(5, 'inquiry', '$2y$10$ZevNen4HRAUP6Ba3omByOeZkx8LSN1eNXhniM.DT9H099rn94pcIq', 'inquiry', 'TestGroup'),
+(7, 'manager', '$2y$10$vjgy3qXYdm.GXnGc7V/k8u8sxhQQc8xUHkWwCO6cQWx.UJnZC6a/i', 'manager', 'TestGroup'),
+(8, 'customer', '$2y$10$EPSockOutL5U7Hv2udhKAOu9GNjQj3ydXSgQQhwf8RUGIK4qOk3CO', 'customer', 'TestGroup');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `form_delivery`
+--
+
+CREATE TABLE `form_delivery` (
+  `id` int(11) NOT NULL,
+  `product_name` varchar(50) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `groupname` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `form_purchase`
+--
+
+CREATE TABLE `form_purchase` (
+  `id` int(11) NOT NULL,
+  `product_name` varchar(50) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `groupname` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `history`
+--
+
+CREATE TABLE `history` (
+  `id` int(11) NOT NULL,
+  `groupname` varchar(50) NOT NULL,
+  `batch_number` varchar(15) NOT NULL,
+  `product_name` varchar(50) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `order_type` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inventory`
+--
+
+CREATE TABLE `inventory` (
+  `id` int(11) NOT NULL,
+  `product_name` varchar(50) NOT NULL,
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `inventory`
+--
+
+INSERT INTO `inventory` (`id`, `product_name`, `quantity`) VALUES
+(1, 'bearings', 5),
+(2, 'shirt', 15),
+(3, 'panths', 25),
+(4, 'shoes', 101),
+(5, 'test', 5);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `form_delivery`
+--
+ALTER TABLE `form_delivery`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `form_purchase`
+--
+ALTER TABLE `form_purchase`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `history`
+--
+ALTER TABLE `history`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `inventory`
+--
+ALTER TABLE `inventory`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `accounts`
+--
+ALTER TABLE `accounts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `form_delivery`
+--
+ALTER TABLE `form_delivery`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `form_purchase`
+--
+ALTER TABLE `form_purchase`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `history`
+--
+ALTER TABLE `history`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `inventory`
+--
+ALTER TABLE `inventory`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
